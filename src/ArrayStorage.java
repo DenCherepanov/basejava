ccd4cce2664dc4e958bddc355545b1ec7aacc2b0
Denis Cherepanov <santon1982@gmail.com>
2020-09-30 00:58:33 +0700

    Переименование переменных, избавление от проверок на null, избавление от лишних итераций в циклах.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,56 +3,43 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int count_storage = storage.length;
+    private int SizeStorage = 0;
 
     void clear() {
-        for (int i = 0; i < count_storage - 1; i++) {
-            if (storage[i] != null) {
+        if (SizeStorage > 0) {
+            for (int i = 0; i < SizeStorage; i++) {
                 storage[i] = null;
-            } else {
-                break;
             }
+            SizeStorage = 0;
         }
     }
 
     void save(Resume r) {
-        for (int i = 0; i < count_storage - 1; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
-        }
+        storage[SizeStorage] = r;
+        SizeStorage++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < count_storage - 1; i++) {
-            if (storage[i] != null) {
+        if (SizeStorage > 0) {
+            for (int i = 0; i < SizeStorage; i++) {
                 if (storage[i].uuid == uuid) {
                     return storage[i];
                 }
-            } else {
-                break;
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int last_resume = -1;
-        int del_resume = -1;
-        for (int i = 0; i < count_storage - 1; i++) {
-            if (storage[i] != null) {
-                last_resume = i;
-            } else {
-                break;
+        if (SizeStorage > 0) {
+            for (int i = 0; i < SizeStorage; i++) {
+                if (storage[i].uuid == uuid) {
+                    storage[i] = storage[SizeStorage - 1];
+                    storage[SizeStorage - 1] = null;
+                    SizeStorage--;
+                    break;
+                }
             }
-            if (storage[i].uuid == uuid) {
-                del_resume = i;
-            }
-        }
-        if (last_resume > -1 & del_resume > -1) {
-            storage[del_resume] = storage[last_resume];
-            storage[last_resume] = null;
         }
     }
 
@@ -60,17 +47,9 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int size_resume = -1;
-        for (int i = 0; i < count_storage - 1; i++) {
-            if (storage[i] != null) {
-                size_resume = i + 1;
-            } else {
-                break;
-            }
-        }
-        if (size_resume != -1) {
-            Resume[] new_storage = new Resume[size_resume];
-            for (int i = 0; i < size_resume; i++) {
+        if (SizeStorage > 0) {
+            Resume[] new_storage = new Resume[SizeStorage];
+            for (int i = 0; i < SizeStorage; i++) {
                 new_storage[i] = storage[i];
             }
             return new_storage;
@@ -80,18 +59,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int size_resume = -1;
-        for (int i = 0; i < count_storage - 1; i++) {
-            if (storage[i] != null) {
-                size_resume = i;
-            } else {
-                break;
-            }
-        }
-        if (size_resume != -1) {
-            return size_resume + 1;
-        } else {
-            return 0;
-        }
+        return SizeStorage;
     }
 }
